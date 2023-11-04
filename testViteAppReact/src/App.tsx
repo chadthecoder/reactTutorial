2f74a864f39c711bd2b6f02fabd5932ca7c9ba06
chadthecoder <mcadamschad@outlook.com>
2023-11-03 21:51:44 -0700

    unsure

--- testViteAppReact/src/App.tsx
+++ testViteAppReact/src/App.tsx
@@ -1,12 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './Components/Header.tsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
