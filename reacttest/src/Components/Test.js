1ffc162b22fd2a92523c511f6c5a4e73b20472b5
chadthecoder <mcadamschad@outlook.com>
2023-12-15 20:04:50 -0800

    created react app to test react stuff

---
+++ reacttest/src/Components/Test.js
@@ -0,0 +1,11 @@
+import './Test.css';
+
+function Test() {
+  return (
+    <div className="Test">
+      <input className="Test-input" type="text"></input>
+    </div>
+  );
+}
+
+export default Test;
